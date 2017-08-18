9ffe0b60bb6f4c3bea361cc510f47e6a73dcd134
austinChappell <Austin.W.Chappell@gmail.com>
2017-08-17 21:44:53 -0500

    working on user auth for postgresql

--- models/user.js
+++ models/user.js
@@ -1,68 +1,28 @@
 const bcrypt = require('bcryptjs');
-      mongoose = require('mongoose');
+const { Client } = require('pg');
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  passwordHash: {
-    type: String,
-    required: true
-  },
-  name: {
-    type: String,
-    required: false
-  },
-  avatar: {
-    type: String,
-    required: false
-  },
-  email: String,
-  university: String,
-  job: String,
-  company: String,
-  skills: [],
-  phone: String,
-  address: {
-    street_num: String,
-    street_name: String,
-    city: String,
-    state_or_province: String,
-    postal_code: String,
-    country: String
-  },
-  messages: []
-});
-
-userSchema.virtual('password').get(function() {
-  return null;
-}).set(function(value) {
-  this.passwordHash = bcrypt.hashSync(value, 10);
-});
-
-userSchema.methods.authenticate = function(password) {
+const checkPassword = function(password) {
   return bcrypt.compareSync(password, this.passwordHash);
 };
 
-userSchema.statics.authenticate = function(username, password, done) {
+const checkUser = function(username, password, done) {
+  const client = new Client()
 
-  this.findOne({ username: username }, (err, user) => {
-    if (err) {
-      console.log('Error attempting to use the static authenticate function.', err);
-      done(err, false);
-    } else if (user && user.authenticate(password)) {
+  client.connect(() => {
+
+    const sql = 'SELECT * FROM usertable WHERE username = $1';
+    const params = [username];
+
+    return client.query(sql, params)
+  }).then((results) => {
+      const user = results.rows[0];
+
+      if (user && checkPassword(password)) {
       console.log('Should be a successful login.', user);
       done(null, user);
     } else {
       console.log('The user probably entered the incorrect password.');
       done(null, false);
-    }
-  })
-
-}
-
-let User = mongoose.model('User', userSchema);
-
-module.exports = User;
+    };
+  });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const bcrypt = require('bcryptjs');
+const { Client } = require('pg');
 
 const User = require('../models/user');
 
@@ -19,41 +21,67 @@ router.get('/signup', (req, res) => {
 });
 
 router.post('/signup', (req, res, next) => {
-  console.log('body', req.body);
-  let skillsArr = req.body.skills.split(';');
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password,
-    name: req.body.name,
-    avatar: req.body.avatarURL,
-    email: req.body.email,
-    university: req.body.university,
-    job: req.body.job,
-    company: req.body.company,
-    skills: skillsArr,
-    phone: req.body.phone,
-    address: {
-      street_num: req.body.street_num,
-      street_name: req.body.street_name,
-      city: req.body.city,
-      state_or_province: req.body.state_or_province,
-      country: req.body.country
-    }
-  });
-  user.save((err) => {
-    if (err) {
-      console.log('There was an error saving the user', err);
-    }
-    next();
-    // res.redirect('/home');
-  });
-}, passport.authenticate('local', {
-  successRedirect: '/directory'
-}));
 
-router.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/');
+  const username = req.body.username,
+        password = req.body.password,
+        name = req.body.name,
+        avatar = req.body.avatarURL,
+        email = req.body.email,
+        university = req.body.university,
+        job = req.body.job,
+        company = req.body.company,
+        phone = req.body.phone;
+
+  const passwordHash = bcrypt.hashSync(password, 10);
+  const client = new Client();
+
+  client.connect().then(() => {
+    const sql = `INSERT INTO usertable (username, passwordhash, name, avatar, email, university, job, company, phone) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`;
+
+    const params = [username, passwordHash, name, avatar, email, university, job, company, phone];
+
+    return client.query(sql, params);
+  }).then(() => {
+    passport.authenticate('local', {
+      successRedirect: '/directory'
+    });
+  });
 });
+
+
+
+
+
+
+
+//
+//
+//
+//   console.log('body', req.body);
+//   let skillsArr = req.body.skills.split(';');
+//   const user = new User({
+//     address: {
+//       street_num: req.body.street_num,
+//       street_name: req.body.street_name,
+//       city: req.body.city,
+//       state_or_province: req.body.state_or_province,
+//       country: req.body.country
+//     }
+//   });
+//   user.save((err) => {
+//     if (err) {
+//       console.log('There was an error saving the user', err);
+//     }
+//     next();
+//     // res.redirect('/home');
+//   });
+// }, passport.authenticate('local', {
+//   successRedirect: '/directory'
+// }));
+//
+// router.get('/logout', (req, res) => {
+//   req.logout();
+//   res.redirect('/');
+// });
 
 module.exports = router;
